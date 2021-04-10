3ed49df09333524c9b7baea83da408c662bcbb80
aka <aka@localhost.localdomain>
2021-04-11 01:55:57 +0300

    Convert List to Dict for count repeated elements in List

--- pdfconvert.py
+++ pdfconvert.py
@@ -19,13 +19,19 @@ with open('sample3.pdf', 'rb') as in_file:
     for page in PDFPage.create_pages(doc):
         interpreter.process_page(page)
 
-#print(output_string.getvalue().split(" "))
+
 string_from_pdf = output_string.getvalue()
 list_of_words = re.findall(r'\w+', string_from_pdf)
 #print(list_of_words)
 print(len(list_of_words))
-print(type(list_of_words))
-list_of_words.sort()
-print(list_of_words)
+print(len(set(list_of_words)))
+#print(set(list_of_words))
+
+#list_of_words.sort()
+#print(list_of_words)
+
+dict_of_words = {i:list_of_words.count(i) for i in list_of_words}
+print(dict_of_words)
+
 
 
