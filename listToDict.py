a8fcba548bcede50de4bc6d279ffaffe12846e76
aka <aka@localhost.localdomain>
2021-04-11 03:32:53 +0300

    Split code to modules

--- listToDict.py
+++ listToDict.py
@@ -1,11 +1,11 @@
-import pdfconvert
+
 
 #dict_of_words = {i:list_of_words.count(i) for i in list_of_words}
 #print(dict_of_words)
 
-def convert_list_to_dict():
-    temp=set(pdfconvert.list_of_words)
+def convert_list_to_dict(list_of_words):
+    temp=set(list_of_words)
     result={}
     for i in temp:
-        result[i]=pdfconvert.list_of_words.count(i)
-    print(result)
+        result[i]=list_of_words.count(i)
+    return result
--- pdfconvert.py
+++ pdfconvert.py
@@ -1,38 +1,11 @@
-import re
-from listToDict import *
 
-from io import StringIO
+import listToDict
+import pdfToStr
+import strToList
 
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfparser import PDFParser
+str_from_pdf = pdfToStr.convert_pdf_to_str()
+list_of_words = strToList.convert_str_to_list(str_from_pdf)
+dict_of_words = listToDict.convert_list_to_dict(list_of_words)
 
-output_string = StringIO()
-with open('sample2.pdf', 'rb') as in_file:
-    parser = PDFParser(in_file)
-    doc = PDFDocument(parser)
-    rsrcmgr = PDFResourceManager()
-    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    for page in PDFPage.create_pages(doc):
-        interpreter.process_page(page)
-
-
-string_from_pdf = output_string.getvalue().lower()
-
-list_of_words = re.findall(r'\w+', string_from_pdf)
-
-#print(list_of_words)
-print(len(list_of_words))
-#print(len(set(list_of_words)))
-#print(set(list_of_words))
-
-#list_of_words.sort()
-#print(list_of_words)
-
-
-
+print(dict_of_words)
 
